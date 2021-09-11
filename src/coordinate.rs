f25676d11b10191baa07de307ff17f5b063a4038
Derrick Plotsky <derrickplotsky@gmail.com>
2021-09-10 20:31:55 -0700

    Add Coordinate
    An x/y value for our cell or anything else.
    
    Also remove useless test.

---
+++ src/coordinate.rs
@@ -0,0 +1,40 @@
+#[derive(Copy, Clone, Eq, Hash, PartialEq, Debug)]
+pub struct Coordinate<T: Copy> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T: Copy> Coordinate<T> {
+    pub fn new(x: T, y: T) -> Self {
+        return Coordinate { x, y };
+    }
+
+    pub fn splat(value: T) -> Self {
+        return Coordinate { x: value, y: value };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod splat {
+        use crate::coordinate::Coordinate;
+
+        #[test]
+        fn it_creates_coordinate_with_same_values() {
+            let coordinate = Coordinate::<i32>::splat(2);
+            assert_eq!(coordinate.x, 2);
+            assert_eq!(coordinate.y, 2);
+        }
+    }
+
+    mod new {
+        use crate::coordinate::Coordinate;
+
+        #[test]
+        fn it_creates_coordinate_properly() {
+            let coordinate = Coordinate::<i32>::new(2, 4);
+            assert_eq!(coordinate.x, 2);
+            assert_eq!(coordinate.y, 4)
+        }
+    }
+}
