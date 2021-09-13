4fa23dced2c37479b3c83b98b45637109776fabd
Derrick Plotsky <derrickplotsky@gmail.com>
2021-09-12 23:40:20 -0700

    Add more to our TopDownMap
    This will help users of this to know how many rooms there are, and give
    them a specific starting coordinate.

--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,7 @@
-use crate::grid::Grid;
+use crate::{coordinate::Coordinate, grid::Grid};
 
 pub struct TopDownMap {
     pub grid: Grid<i32>,
+    pub room_count: usize,
+    pub entry_coordinate: Coordinate<i32>,
 }
