19cacc3697430f5f0dec22f921c829eabdde7e9e
Derrick Plotsky <derrickplotsky@gmail.com>
2021-09-12 22:56:45 -0700

    Start on the generator
    This will be what I use to generate a single top down map.

---
+++ src/generator.rs
@@ -0,0 +1,4 @@
+pub struct Generator {
+    pub grid_size: usize,
+    pub target_number_rooms: usize,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod cell;
 pub mod coordinate;
+pub mod generator;
 pub mod grid;
 pub mod map;
 pub mod room;
