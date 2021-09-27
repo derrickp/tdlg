9f0163f7f7097763bffc9c9e6d2a6491650f9ea4
Derrick Plotsky <derrickplotsky@gmail.com>
2021-09-26 22:00:48 -0700

    Move room loading to a struct
    Eventually I am going to try to get rooms to have items and other
    environmental effects in them. These will come in from other files
    and this struct lets me separate all of the loading into multiple
    folders much easier.

--- src/generator.rs
+++ src/generator.rs
@@ -1,37 +1,54 @@
-use crate::{grid::Grid, loading::load_room_templates, map::TopDownMap};
+use crate::{grid::Grid, loading::RoomPaths, map::TopDownMap, room::Room};
 use rand::Rng;
 
 pub struct Generator {
     pub grid_size: usize,
     pub target_number_rooms: usize,
-    pub room_template_directory: &'static str,
+    pub all_room_paths: Vec<RoomPaths>,
 }
 
 impl Generator {
-    pub fn generate_top_down_map(&self) -> TopDownMap {
+    pub fn generate_top_down_map(&self) -> Option<TopDownMap> {
+        if self.all_room_paths.is_empty() {
+            return None;
+        }
+
+        let room_iterator = self
+            .all_room_paths
+            .iter()
+            .map(|f| f.load_rooms().into_iter().flatten())
+            .flatten();
+        let mut room_templates: Vec<Room<i32>> = Vec::new();
+
+        for room in room_iterator {
+            room_templates.push(room);
+        }
+
+        if room_templates.is_empty() {
+            return None;
+        }
+
         let mut rng = rand::thread_rng();
 
         let mut grid = Grid::build(self.grid_size);
         let mut room_count = 0;
 
-        if let Some(room_templates) = load_room_templates(self.room_template_directory) {
-            for _ in 0..self.target_number_rooms {
-                let index: usize = rng.gen_range(0..room_templates.len());
-                let template = room_templates.get(index).unwrap().clone();
-                let max_side_length = template.max_side_length;
+        for _ in 0..self.target_number_rooms {
+            let index: usize = rng.gen_range(0..room_templates.len());
+            let template = room_templates.get(index).unwrap().clone();
+            let max_side_length = template.max_side_length;
 
-                let x: i32 = rng.gen_range(1..=(self.grid_size - max_side_length) as i32);
-                let y: i32 = rng.gen_range(1..=(self.grid_size - max_side_length - 1) as i32);
-                let room = template.translate(x, y);
+            let x: i32 = rng.gen_range(1..=(self.grid_size - max_side_length) as i32);
+            let y: i32 = rng.gen_range(1..=(self.grid_size - max_side_length - 1) as i32);
+            let room = template.translate(x, y);
 
-                if room
-                    .cells
-                    .iter()
-                    .all(|cell| grid.is_cell_empty(&cell.coordinate))
-                {
-                    room_count += 1;
-                    grid.add_room(room);
-                }
+            if room
+                .cells
+                .iter()
+                .all(|cell| grid.is_cell_empty(&cell.coordinate))
+            {
+                room_count += 1;
+                grid.add_room(room);
             }
         }
 
@@ -40,10 +57,10 @@ impl Generator {
 
         let entry_coordinate = grid.random_spawnable_coordinate().unwrap();
 
-        TopDownMap {
+        Some(TopDownMap {
             grid,
             room_count,
             entry_coordinate,
-        }
+        })
     }
 }
--- src/loading.rs
+++ src/loading.rs
@@ -2,22 +2,33 @@ use crate::room::Room;
 use std::fs;
 use walkdir::WalkDir;
 
-pub fn load_room_templates(path: &str) -> Option<Vec<Room<i32>>> {
-    let mut templates: Vec<String> = Vec::new();
-    for entry in WalkDir::new(path).into_iter().flatten() {
-        if let Ok(content) = fs::read_to_string(entry.path()) {
-            templates.push(content);
-        }
-    }
+pub struct RoomPaths {
+    pub name: &'static str,
+    pub base_template_path: &'static str,
+    pub fill_template_path: &'static str,
+}
 
-    if templates.is_empty() {
-        None
-    } else {
-        Some(
-            templates
-                .iter()
-                .map(|template_string| Room::<i32>::from_template_string(template_string.clone()))
-                .collect(),
-        )
+impl RoomPaths {
+    pub fn load_rooms(&self) -> Option<Vec<Room<i32>>> {
+        let mut templates: Vec<String> = Vec::new();
+
+        for entry in WalkDir::new(self.base_template_path).into_iter().flatten() {
+            if let Ok(content) = fs::read_to_string(entry.path()) {
+                templates.push(content);
+            }
+        }
+
+        if templates.is_empty() {
+            None
+        } else {
+            Some(
+                templates
+                    .iter()
+                    .map(|template_string| {
+                        Room::<i32>::from_template_string(template_string.clone())
+                    })
+                    .collect(),
+            )
+        }
     }
 }
