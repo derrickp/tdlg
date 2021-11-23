74598b1f62acb014575f2b3076e275a00f296e8f
Derrick Plotsky <derrickplotsky@gmail.com>
2021-11-22 22:54:13 -0800

    Update layer types
    This adds a few more layer types so more things can be spawned within
    rooms. Also adding From for `char` so that all the code for that is
    contained within the same module.

--- src/cells/layer.rs
+++ src/cells/layer.rs
@@ -8,17 +8,49 @@ pub enum LayerType {
     RoomWall,
     Rubble,
     Table,
+    Structure,
+    CommonItem,
+    UncommonItem,
+    RareItem,
+    ExoticItem,
+    Note,
+}
+
+impl From<char> for LayerType {
+    fn from(c: char) -> Self {
+        match c {
+            'w' => LayerType::RoomWall,
+            'f' => LayerType::RoomFloor,
+            'd' => LayerType::Door,
+            'r' => LayerType::Rubble,
+            't' => LayerType::Table,
+            'l' => LayerType::Floor,
+            'e' => LayerType::Empty,
+            'o' => LayerType::OuterWall,
+            's' => LayerType::Structure,
+            'c' => LayerType::CommonItem,
+            'u' => LayerType::UncommonItem,
+            'a' => LayerType::RareItem,
+            'x' => LayerType::ExoticItem,
+            'n' => LayerType::Note,
+            _ => LayerType::Empty,
+        }
+    }
 }
 
 impl LayerType {
     pub fn is_walkable(&self) -> bool {
-        self == &LayerType::Door
-            || self == &LayerType::RoomFloor
-            || self == &LayerType::Floor
-            || self == &LayerType::Rubble
+        let walkable_types = vec![
+            LayerType::Door,
+            LayerType::RoomFloor,
+            LayerType::Floor,
+            LayerType::RoomFloor,
+        ];
+        walkable_types.contains(self)
     }
 
     pub fn is_spawnable(&self) -> bool {
-        self == &LayerType::RoomFloor || self == &LayerType::Floor
+        let spawnable_types = vec![LayerType::RoomFloor, LayerType::Floor];
+        spawnable_types.contains(self)
     }
 }
